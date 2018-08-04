85c72960fbc243c66e1023ff4583d4c2ad579b41
Pavel K <pietrovich@users.noreply.github.com>
2018-08-04 16:40:12 +0300

    wip. added *.d.ts to processed files

--- src/index.ts
+++ src/index.ts
@@ -8,69 +8,84 @@ import * as xregexp from 'xregexp';
 const HAS_BACKSLASH_SEPARATOR = Path.sep === '\\';
 
 try {
-	require('source-map-support').install();
+    require('source-map-support').install();
 } catch (error) {
-	console.warn('Source map support unavailable ("source-map-support" required). Proceeding without.');
+    console.warn('Source map support unavailable ("source-map-support" required). Proceeding without.');
 }
 
-const convertPaths = (conversionTargetRoot?: string) => {
+const matchers = [
+    /\.js$/,
+    /\.d\.ts$/,
+];
+const ignoreMatcher = (file:string, stats:Fs.Stats) => {
+    for (let rx of matchers) {
+        if (rx.test(file)) {
+            // don't ignore matched paths
+            return false;
+        }
+    }
 
-	if (!conversionTargetRoot) throw Error('Could not determine conversion target root');
+    return true;
+};
 
-	recursive(conversionTargetRoot, ['!*.js'], (error, files) => {
-		files.forEach(file => {
-			const fileContent = Fs.readFileSync(file, 'utf-8');
-			const regex = xregexp(/require\(["']([^"']*)["']\)/);
-			const modifiedFileContent = xregexp.replace(
-				fileContent,
-				regex,
-				(wholeRequire: string, modulePath: string) => {
-					try {
-						require(modulePath);
-						return wholeRequire;
-					} catch (error) {
-						let newRequirePath = Path.relative(`${file}/..`, `${conversionTargetRoot}/${modulePath}`);
-						if (newRequirePath[0] !== '.') newRequirePath = './' + newRequirePath;
-						// Fix backslash path separator
-						if (HAS_BACKSLASH_SEPARATOR) {
-							newRequirePath = newRequirePath.replace(/\\/g, '/');
-						}
-						return `require("${newRequirePath}")`;
-					}
-				},
-				'all',
-			);
-			Fs.writeFileSync(file, modifiedFileContent);
-		});
-	});
+const convertPaths = (conversionTargetRoot?:string) => {
+
+    if (!conversionTargetRoot) throw Error('Could not determine conversion target root');
+
+    recursive(conversionTargetRoot, ignoreMatcher, (error, files) => {
+        files.forEach(file => {
+            const fileContent = Fs.readFileSync(file, 'utf-8');
+            const regex = xregexp(/require\(["']([^"']*)["']\)/);
+            const modifiedFileContent = xregexp.replace(
+                fileContent,
+                regex,
+                (wholeRequire:string, modulePath:string) => {
+                    try {
+                        require(modulePath);
+                        return wholeRequire;
+                    } catch (error) {
+                        let newRequirePath = Path.relative(`${file}/..`, `${conversionTargetRoot}/${modulePath}`);
+                        if (newRequirePath[0] !== '.') newRequirePath = './' + newRequirePath;
+                        // Fix backslash path separator
+                        if (HAS_BACKSLASH_SEPARATOR) {
+                            newRequirePath = newRequirePath.replace(/\\/g, '/');
+                        }
+                        return `require("${newRequirePath}")`;
+                    }
+                },
+                'all',
+            );
+            Fs.writeFileSync(file, modifiedFileContent);
+        });
+    });
 };
 
 
 const parentProjectRoot = Path.resolve(__dirname, '../../../');
 
-let conversionTargetRoot: string | undefined;
+let conversionTargetRoot:string|undefined;
 
 const conversionTargetArgument = process.argv[2];
 
 if (conversionTargetArgument) {
-	conversionTargetRoot = Path.resolve(parentProjectRoot, conversionTargetArgument);
+    conversionTargetRoot = Path.resolve(parentProjectRoot, conversionTargetArgument);
 } else {
-	try {
-		/**
-		* Currently support only outDir parameter of tsconfig.json
-		* for determining single module root
-		*
-		* TODO 1: multiple module root support
-		* TODO 2: support also other module path information sources in addition to tsconfig.json
-		*/
-		const tsconfigPath = Path.resolve(parentProjectRoot, 'tsconfig.json');
-		const tsconfig = require(tsconfigPath);
-		if (tsconfig.compilerOptions && tsconfig.compilerOptions.outDir) {
-			conversionTargetRoot = Path.resolve(parentProjectRoot, tsconfig.compilerOptions.outDir);
-		}
-		convertPaths(conversionTargetRoot);
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        /**
+         * Currently support only outDir parameter of tsconfig.json
+         * for determining single module root
+         *
+         * TODO 1: multiple module root support
+         * TODO 2: support also other module path information sources in addition to tsconfig.json
+         */
+        const tsconfigPath = Path.resolve(parentProjectRoot, 'tsconfig.json');
+        const tsconfig = require(tsconfigPath);
+        if (tsconfig.compilerOptions && tsconfig.compilerOptions.outDir) {
+            conversionTargetRoot = Path.resolve(parentProjectRoot, tsconfig.compilerOptions.outDir);
+        }
+        convertPaths(conversionTargetRoot);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
